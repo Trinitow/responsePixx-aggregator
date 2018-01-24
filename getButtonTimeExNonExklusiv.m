f7f456acba982ba4ca524933002475964fb7ca13
Jan Grenzebach <30291513+Trinitow@users.noreply.github.com>
2018-01-24 11:38:26 +0100

    Update getButtonTimeExNonExklusiv.m

--- getButtonTimeExNonExklusiv.m
+++ getButtonTimeExNonExklusiv.m
@@ -1,9 +1,14 @@
 function a = getButtonTimeExNonExklusiv(buttonStates, tgetsecs, tStart, tEnd)
-% a gibt die Zeiten fuer alle Tastenkombinationen aus, die aufgetreten sind.
-% Jede Zeile entspricht einer Kombination. 5 Spalten fuer die Tasten und eine Spalte fuer dir Zeit
+% IN:
+% buttonStates: can be retrieved from responsePixx after the experimental block was finished 
+% tgetsecs: can be retrieved from responsePixx after the experimental block was finished - global times
+% tStart: global start time of the block (save with block beginning as GetSecs)
+% tEnd: global end time of the block (save with block ending as GetSecs)
+% OUT: 
+% a contains all unique key-press-states in the buttonStates manner (col=1:5) + total time of this state (col=6)
+% every Row contains one unique sttes
 buttonStates = vertcat([0 0 0 0 0], buttonStates, [0 0 0 0 0]);
 tgetsecs= [tStart tgetsecs tEnd];
-% Matrix erstellen, in der alle Kombinationen von Tastendruecken stehen, die auftauchen. In der sechsten Spalte steht die Zeit
 a=unique(buttonStates,'rows');
 buttonStates (1:end-1,6) = tgetsecs(2:end)-tgetsecs(1:end-1); % Zeiten der einzelnen Zustaende berechnen
 % Zeiten berechnen
@@ -11,6 +16,5 @@ for r=1:size(a,1)
     [~, index]=ismember(buttonStates(:,1:5),a(r,:),'rows');
     uniqueCombinationsTimePressed(r) = sum(buttonStates(find(index),6));
 end
-% Sechste Spalte mit Zeiten fuellen
 a(:,6)=uniqueCombinationsTimePressed;
 end
